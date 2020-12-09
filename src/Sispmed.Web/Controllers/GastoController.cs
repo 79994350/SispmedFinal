21795bc1da1a9570cfa1d1e739f7de69b7ae9f64
79994350 <67530498+79994350@users.noreply.github.com>
2020-12-09 17:04:52 -0500

    s

---
+++ src/Sispmed.Web/Controllers/GastoController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sispmed.Core.Domain;
+using Sispmed.Infrastructure.Data;
+
+namespace Sispmed.Web.Controllers
+{
+    public class GastoController : Controller
+    {
+        private readonly SispmedDbContext _context;
+
+        public GastoController(SispmedDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Gasto
+        public async Task<IActionResult> Index()
+        {
+            var sispmedDbContext = _context.Gastos.Include(g => g.Empleado);
+            return View(await sispmedDbContext.ToListAsync());
+        }
+
+        // GET: Gasto/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gastos = await _context.Gastos
+                .Include(g => g.Empleado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gastos == null)
+            {
+                return NotFound();
+            }
+
+            return View(gastos);
+        }
+
+        // GET: Gasto/Create
+        public IActionResult Create()
+        {
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id");
+            return View();
+        }
+
+        // POST: Gasto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Concepto,FecGasto,ForPago,Valor,EmpleadoId")] Gastos gastos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(gastos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id", gastos.EmpleadoId);
+            return View(gastos);
+        }
+
+        // GET: Gasto/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gastos = await _context.Gastos.FindAsync(id);
+            if (gastos == null)
+            {
+                return NotFound();
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id", gastos.EmpleadoId);
+            return View(gastos);
+        }
+
+        // POST: Gasto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Concepto,FecGasto,ForPago,Valor,EmpleadoId")] Gastos gastos)
+        {
+            if (id != gastos.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gastos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GastosExists(gastos.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EmpleadoId"] = new SelectList(_context.Empleados, "Id", "Id", gastos.EmpleadoId);
+            return View(gastos);
+        }
+
+        // GET: Gasto/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gastos = await _context.Gastos
+                .Include(g => g.Empleado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gastos == null)
+            {
+                return NotFound();
+            }
+
+            return View(gastos);
+        }
+
+        // POST: Gasto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var gastos = await _context.Gastos.FindAsync(id);
+            _context.Gastos.Remove(gastos);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GastosExists(int id)
+        {
+            return _context.Gastos.Any(e => e.Id == id);
+        }
+    }
+}
